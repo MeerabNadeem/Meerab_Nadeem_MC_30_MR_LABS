d4ad336764f36198c1abcb23c99047e0de7508f2
MeerabNadeem <mirab5129@gmail.com>
2026-02-27 13:03:07 +0500

    Add files via upload

---
+++ Week1/my_first_package/simple_node.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+
+class SimpleNode(Node):
+    def __init__(self):
+        super().__init__('simple_node')
+
+        # Declare parameter with default value None
+        self.declare_parameter('student_name', None)
+
+        # Get parameter value
+        student_name = self.get_parameter('student_name').value
+
+        # Check if parameter is set
+        if student_name:
+            self.get_logger().info(f'Student name: {student_name}')
+        else:
+            self.get_logger().info('student_name not set')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SimpleNode()
+
+    rclpy.spin_once(node, timeout_sec=0.1)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
